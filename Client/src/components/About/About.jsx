63268e64c385173098ffa366584b3c4fd9df52c3
Gabriel Benevento <gaby88akd@gmail.com>
2023-06-23 16:48:28 -0300

    estilos

--- Client/src/components/About/About.jsx
+++ Client/src/components/About/About.jsx
@@ -2,14 +2,18 @@ import img from '../../assets/hqdefault.jpg';
 import style from './About.module.css';
 
 const About = () => {
-  return (
-    <div className={style.body}>
-      <img src={img} alt='' />
-      <div className={style.description}>
-        <h1>Me llamo Gabriel Benevento, tengo 27 años y blablabla...</h1>
-      </div>
-    </div>
-  );
+	return (
+		<div className={style.body}>
+			<img src={img} alt='' />
+			<div className={style.description}>
+				<h1>Me llamo Gabriel Benevento</h1>
+				<h2>
+					soy un estuadiante de Henry, al que le gusta programar y que se esta
+					esforzando por combertir algo que le gusta en su profesion.
+				</h2>
+			</div>
+		</div>
+	);
 };
 
 export default About;
