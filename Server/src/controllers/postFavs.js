3e5a34864d45475cd879f5e1e54170530bb24332
Gabriel Benevento <gaby88akd@gmail.com>
2023-06-30 18:07:46 -0300

    todo funcional

--- Server/src/controllers/postFavs.js
+++ Server/src/controllers/postFavs.js
@@ -2,13 +2,16 @@ const { Favorite } = require('../DB_connection');
 
 module.exports = async (req, res) => {
 	try {
-		const { id, name, origin, status, image, species, gender } = req.body;
-
-		if (!name || !origin || !status || !image || !species || !gender)
+		const { id, name, image, species, gender } = req.body;
+		console.log(name);
+		console.log(image);
+		console.log(species);
+		console.log(gender);
+		if (!id || !name || !image || !species || !gender)
 			return res.status(401).send('Faltan datos');
 
 		await Favorite.findOrCreate({
-			where: { name, origin, status, image, species, gender },
+			where: { id, name, image, species, gender },
 		});
 		const allFav = await Favorite.findAll();
 		return res.status(200).json(allFav);
